c47c10b7b1e69197639fc7aee50e99ee13fbfdd4
Rizal muhamad H <rizalmuhamadh@gmail.com>
2022-09-07 14:05:23 +0700

    Initial commit

---
+++ main.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"net/http"
+	"strconv"
+
+	"github.com/apex/log"
+	"github.com/apex/log/handlers/cli"
+)
+
+func run() {
+	log.SetHandler(cli.Default)
+	log.SetLevel(log.DebugLevel)
+
+	rmq, err := initRabbitMQ()
+	if err != nil {
+		log.Fatalf("run: failed to init rabbitmq: %v", err)
+	}
+	defer rmq.Shutdown()
+
+	// err = rmq.PublishWithDelay("user.event.publish", []byte("Haii guys!!"), 10000)
+	// if err != nil {
+	// 	log.Fatalf("run: failed to publish into rabbitmq: %v", err)
+	// }
+
+	for {
+	}
+}
+
+func runServer() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/schedule/request", func(w http.ResponseWriter, r *http.Request) {
+		param := r.URL.Query()
+		if len(param) != 0 {
+			id := param["id"][0]
+			timestamp, err := strconv.ParseInt(param["timestamp"][0], 0, 64)
+
+			rmq, err := initRabbitMQ()
+			if err != nil {
+				log.Fatalf("run: failed to init rabbitmq: %v", err)
+			}
+			defer rmq.Shutdown()
+
+			err = rmq.PublishWithDelay("user.event.publish", []byte(id), timestamp)
+			if err != nil {
+				log.Fatalf("run: failed to publish into rabbitmq: %v", err)
+			}
+
+			w.Write([]byte("success"))
+		} else {
+			w.Write([]byte("failed"))
+		}
+
+	})
+
+	server := http.Server{
+		Addr:    ":5790",
+		Handler: mux,
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		panic(err)
+	}
+}
+
+func main() {
+	go runServer()
+	run()
+
+}
